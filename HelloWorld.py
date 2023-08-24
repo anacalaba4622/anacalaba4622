db75ccb42712e65bfae2fbbf696f422b5b78f704
anacalaba4622 <143122194+anacalaba4622@users.noreply.github.com>
2023-08-24 13:58:12 -0500

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,15 @@
+# Full Name:Aljon Nacalaban
+# Student ID:1104622
+# Assignment Due Date:Sunday,Aug 27,2023 at 11:59 pm CST
+# MSITM 6341:FA01-2023
+# Assignment #1
+# Assignment Title: Hello World
+# HelloWorld.py
+
+print ("Hello World!")
+print()
+print("My name is Aljon Nacalaban")
+print()
+print("I am a Python Student")
+print()
+print("Have a nice day!")
